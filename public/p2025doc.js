eec941b6556b501e3f27cee343b386c5a760004a
Benjamin Rahn <235302+brahn@users.noreply.github.com>
2024-08-01 18:45:40 -0700

    use page numbers in anchor links if available

--- public/p2025doc.js
+++ public/p2025doc.js
@@ -1,3 +1,50 @@
+const getPageNumber = (pageEl) => {
+  if (!pageEl || !pageEl.firstChild) {
+    if (pageEl.id === "pf30") {
+      console.log("A");
+    }
+    return;
+  }
+  const firstChild = pageEl.firstChild;
+
+  if (
+    firstChild.nodeType === Node.ELEMENT_NODE &&
+    firstChild.tagName === "DIV"
+  ) {
+    if (pageEl.id === "pf30") {
+      console.log("B");
+    }
+    const textContent = firstChild.textContent;
+
+    const match = textContent.match(/— (\w+) —/);
+    if (match) {
+      if (pageEl.id === "pf30") {
+        console.log("C");
+      }
+      return match[1];
+    }
+    if (pageEl.id === "pf30") {
+      console.log("D");
+    }
+  }
+  if (pageEl.id === "pf30") {
+    console.log("E");
+  }
+
+  return;
+};
+
+const insertAnchorTag = (el, anchorName) => {
+  const anchorTag = document.createElement("a");
+  anchorTag.id = anchorName;
+  anchorTag.name = anchorName;
+  if (el.firstChild) {
+    el.insertBefore(anchorTag, el.firstChild);
+  } else {
+    el.appendChild(anchorTag);
+  }
+};
+
 const copyAnchorLink = (anchorName) => {
   // Get the full link
   const currentUrl = window.location.href;
@@ -21,11 +68,20 @@ const copyAnchorLink = (anchorName) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("#page-container > div").forEach((el) => {
+    let anchorName = el.id;
+
+    // use the page number as the anchor name if available
+    const pageNumber = getPageNumber(el);
+    if (pageNumber) {
+      anchorName = `p-${pageNumber}`;
+      insertAnchorTag(el, anchorName);
+    }
+
     const button = document.createElement(`button`);
     button.style = "position: absolute; right: 20px; top: 20px";
     button.innerHTML = "Copy page link";
     el.appendChild(button);
 
-    button.addEventListener("click", (e) => copyAnchorLink(el.id), false);
+    button.addEventListener("click", (e) => copyAnchorLink(anchorName), false);
   });
 });
